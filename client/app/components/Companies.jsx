c7d2cdf0df3123eae42a67cce7443586d5af42f4
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-05-22 18:04:34 +0600

    trusted companies section done for heroSection

--- client/app/components/Companies.jsx
+++ client/app/components/Companies.jsx
@@ -1,5 +1,26 @@
-const Companies = () => {
-  return <div>Companies</div>;
-};
+import Image from 'next/image';
+
+function Companies() {
+  return (
+    <div className="flex items-center justify-center text-gray-400 text-2xl font-bold min-h-[11vh] ">
+      <span className="text-base font-semibold  tracking-wide">
+        Trusted by:
+      </span>
+      &nbsp;
+      <ul className="flex justify-between gap-10 ml-10">
+        {[1, 2, 3, 5].map((num) => (
+          <li key={num} className="relative h-[4.5rem] w-[4.5rem]">
+            <Image
+              src={`/trusted${num}.png`}
+              alt="trusted brands"
+              fill
+              className="w-full h-full"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Companies;
--- client/app/components/HeroBanner.jsx
+++ client/app/components/HeroBanner.jsx
@@ -1,11 +1,20 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const HeroBanner = () => {
   // const router = useRouter();
-  const [image, setImage] = useState(1);
+  const [image, setImage] = useState(3);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setImage(image >= 3 ? 1 : image + 1),
+      10000
+    );
+    return () => clearInterval(interval);
+  }, [image]);
 
   return (
     <div className="hero-banner h-[680px] relative bg-cover">
@@ -18,6 +27,64 @@ const HeroBanner = () => {
             image === 1 ? 'opacity-100' : 'opacity-0'
           } transition-all duration-1000`}
         />
+        <Image
+          alt="hero-image"
+          src="https://i.ibb.co/WfpSC3S/bg-hero2.webp"
+          fill
+          className={`${
+            image === 2 ? 'opacity-100' : 'opacity-0'
+          } transition-all duration-1000`}
+        />
+        <Image
+          alt="hero-image"
+          src="https://i.ibb.co/b6hqdrL/bg-hero5.webp"
+          fill
+          className={`${
+            image === 3 ? 'opacity-100' : 'opacity-0'
+          } transition-all duration-1000`}
+        />
+      </div>
+      <div className="z-20 relative w-[650px] flex justify-center flex-col h-full gap-5 ml-20">
+        <h1 className="text-white text-5xl font-semibold leading-snug">
+          Find the perfect&nbsp;
+          <span className="font-domine font-light">
+            freelance
+            <br />
+            service,
+          </span>
+          &nbsp;right away
+        </h1>
+        <div className="flex align-middle">
+          <div className="relative">
+            <input
+              type="text"
+              className="h-12 w-[550px] pl-4 rounded-md rounded-r-none"
+              placeholder="Search for any service..."
+            />
+          </div>
+          <button className="bg-[#1DBF73] text-white px-6 text-lg font-semibold rounded-r-md">
+            <span className="text-2xl">
+              <AiOutlineSearch />
+            </span>
+          </button>
+        </div>
+        <div className="text-white flex items-center gap-4">
+          <span className="text-sm font-semibold">Popular:</span>
+          <ul className="flex gap-4">
+            <li className="text-sm font-medium py-1 px-3 border rounded-full hover:bg-white hover:text-black hover:font-semibold transition-all duration-300 cursor-pointer">
+              Website Design
+            </li>
+            <li className="text-sm font-medium py-1 px-3 border rounded-full hover:bg-white hover:text-black hover:font-semibold transition-all duration-300 cursor-pointer">
+              Wordpress
+            </li>
+            <li className="text-sm font-medium py-1 px-3 border rounded-full hover:bg-white hover:text-black hover:font-semibold transition-all duration-300 cursor-pointer">
+              Logo Design
+            </li>
+            <li className="text-sm font-medium py-1 px-3 border rounded-full hover:bg-white hover:text-black hover:font-semibold transition-all duration-300 cursor-pointer">
+              AI Service
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
